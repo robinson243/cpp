34bd44d95cb05810acca21ccf60779759635bfb7
Robinson Mukena Zevo <romukena@f1r13s5.clusters.42paris.fr>
2026-01-08 18:21:55 +0100

    arg

---
+++ cpp02/ex01/Fixed.cpp
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/07 18:08:36 by romukena          #+#    #+#             */
+/*   Updated: 2026/01/08 18:21:15 by romukena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	_integer = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& object) : _integer(object.getRawBits())
+{
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed& Fixed::operator=(const Fixed& object)
+{
+	if (this != &object)
+    {
+		this->_integer = object.getRawBits();
+    }
+	std::cout << "Copy assignment operator called" << std::endl;
+    return *this;
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return _integer;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	_integer = raw;
+}
+
+Fixed::Fixed(const int num) :_integer(num << _numberBits)
+{
+	std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float num)
+{
+	_integer = roundf(num * (1 << _numberBits));
+	std::cout << "Float constructor called" << std::endl;
+}
+
+float Fixed::toFloat(void) const
+{
+	float floatNumber = _integer / (float)(1 << _numberBits);
+	return (floatNumber);
+}
---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/07 18:06:09 by romukena          #+#    #+#             */
+/*   Updated: 2026/01/08 17:59:01 by romukena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <math.h>
+
+class Fixed
+{
+private:
+	int _integer;
+	const static int _numberBits;
+
+public:
+	Fixed();
+	~Fixed();
+	Fixed(const Fixed &object);
+	Fixed &operator=(const Fixed &object);
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+	float toFloat(void) const;
+	Fixed(const int num);
+	Fixed(const float num);
+};
+
+#endif
