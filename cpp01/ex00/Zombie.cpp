9471dead5e009b20c875eca4a4e2c4ab35f60618
Robinson Mukena Zevo <romukena@f2r8s13.clusters.42paris.fr>
2026-01-15 17:42:33 +0100

    modify some stuff

--- cpp01/ex00/Zombie.cpp
+++ cpp01/ex00/Zombie.cpp
@@ -6,25 +6,24 @@
 /*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/31 01:27:13 by romukena          #+#    #+#             */
-/*   Updated: 2025/12/31 18:48:49 by romukena         ###   ########.fr       */
+/*   Updated: 2026/01/15 17:40:52 by romukena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie(){}
+Zombie::Zombie() {
+}
 
-Zombie::Zombie(std::string _name)
-{
+Zombie::Zombie(std::string _name) {
 	name = _name;
 }
 
-Zombie::~Zombie()
-{
+Zombie::~Zombie() {
 	std::cout << name << " is deleted" << std::endl;
 }
 
-void Zombie::announce(void)
-{
-	std::cout << name << ":" << " BraiiiiiiinnnzzzZ..." << std::endl;
+void Zombie::announce(void) {
+	if (!name.empty())
+		std::cout << name << ":" << " BraiiiiiiinnnzzzZ..." << std::endl;
 }
--- cpp01/ex01/Zombie.cpp
+++ cpp01/ex01/Zombie.cpp
@@ -6,25 +6,22 @@
 /*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/31 18:28:46 by romukena          #+#    #+#             */
-/*   Updated: 2025/12/31 18:50:44 by romukena         ###   ########.fr       */
+/*   Updated: 2026/01/15 17:40:41 by romukena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 Zombie::Zombie() {
-
 }
 Zombie::~Zombie() {
-	
 }
 
-void Zombie::setName(std::string newName)
-{
+void Zombie::setName(std::string newName) {
 	name = newName;
 }
 
-void Zombie::announce(void)
-{
-	std::cout << name << ":" << " BraiiiiiiinnnzzzZ..." << std::endl;
+void Zombie::announce(void) {
+	if (!name.empty())
+		std::cout << name << ":" << " BraiiiiiiinnnzzzZ..." << std::endl;
 }
