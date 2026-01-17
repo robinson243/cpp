115b6b64165b519bb2df684a07ac9dbf37fe7b10
Robinson Mukena Zevo <romukena@f1r8s8.clusters.42paris.fr>
2026-01-17 11:26:09 +0100

    finish i think

---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/15 00:15:42 by romukena          #+#    #+#             */
+/*   Updated: 2026/01/17 11:16:01 by romukena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap
+{
+private:
+	std::string _name;
+	unsigned int _hitPoints;
+	unsigned int _energyPoints;
+	unsigned int _attackDamage;
+
+public:
+	ClapTrap(std::string _Nname);
+	ClapTrap();
+	~ClapTrap();
+	ClapTrap &operator=(const ClapTrap &other);
+	void attack(const std::string &target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+};
+
+#endif
