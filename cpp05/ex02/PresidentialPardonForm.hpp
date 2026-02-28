4bf62b764c9b9d30757abec04515beaf7cb9f117
romukena <romukena@f1br6s9.paris.42.school>
2026-02-28 11:38:49 +0100

    add presidential.hpp

---
+++ cpp05/ex02/PresidentialPardonForm.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PresidentialPardonForm.hpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/28 11:33:30 by romukena          #+#    #+#             */
+/*   Updated: 2026/02/28 11:38:25 by romukena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRESIDENTIALPARDONFORM_HPP
+#define PRESIDENTIALPARDONFORM_HPP
+
+#include <iostream>
+#include "Aform.hpp"
+#include "Bureaucrat.hpp"
+
+class PresidentialPardonForm : public Aform {
+  public:
+	PresidentialPardonForm();
+	~PresidentialPardonForm();
+	PresidentialPardonForm(const PresidentialPardonForm &other);
+	PresidentialPardonForm &operator=(const PresidentialPardonForm &other);
+	PresidentialPardonForm(const std::string &target);
+	void execute(Bureaucrat const &executor) const;
+};
+
+#endif
