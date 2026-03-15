c97bec6a3db911ed62e4b9196560b6768bcb17b5
Robinson Mukena Zevo <romukena@f4r2s12.clusters.42paris.fr>
2026-03-15 18:46:19 +0100

    need to start

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/15 18:36:21 by romukena          #+#    #+#             */
+/*   Updated: 2026/03/15 18:44:30 by romukena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+template <typename T>
+int easyfind(T &first, int n) {
+	if (!first || n < 0)
+		return -1;
+}
+
+#endif
