ce4fd65cb2ea53099fb08cec8ca463ebc7cb8667
mukena_r <robinsonmukena@gmail.com>
2026-03-16 15:31:17 +0100

    finish ex00

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,17 +6,27 @@
 /*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/15 18:36:21 by romukena          #+#    #+#             */
-/*   Updated: 2026/03/15 18:44:30 by romukena         ###   ########.fr       */
+/*   Updated: 2026/03/16 15:28:26 by romukena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdexcept>
 
 template <typename T>
-int easyfind(T &first, int n) {
-	if (!first || n < 0)
-		return -1;
+typename T::iterator easyfind(T &container, int n)
+{
+	std::vector<int>::iterator it = find(container.begin(), container.end(), n);
+	if (it == container.end())
+	{
+		throw std::invalid_argument("Don't find");
+	}
+	else
+		return it;
 }
 
 #endif
---
+++ cpp08/ex00/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/16 14:49:12 by romukena          #+#    #+#             */
+/*   Updated: 2026/03/16 15:30:49 by romukena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+
+int main(void)
+{
+	std::vector<int> vec;
+
+	vec.push_back(5);
+	vec.push_back(7);
+	vec.push_back(1);
+	vec.push_back(54);
+	vec.push_back(18);
+
+	try
+	{
+		std::vector<int>::iterator it = easyfind(vec, 4);
+		std::cout << *it << std::endl;
+	}
+	catch (const std::invalid_argument &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	return 0;
+}
