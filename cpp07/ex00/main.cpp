c2243e336b778f364398c482da08e88eceb83eec
mukena_r <robinsonmukena@gmail.com>
2026-03-17 12:42:43 +0100

    correct main ex00

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 17:25:09 by romukena          #+#    #+#             */
-/*   Updated: 2026/03/13 23:16:20 by romukena         ###   ########.fr       */
+/*   Updated: 2026/03/17 12:39:59 by romukena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,19 @@
 #include "swap.hpp"
 #include "max.hpp"
 
-int main(void) {
-	int a = 25;
-	int b = 55;
-	std::cout << max(a, b) << std::endl;
+int main(void)
+{
+	int a = 2;
+	int b = 3;
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
 	return 0;
 }
