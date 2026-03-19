2ec505dee8bfd83f4f4d63e3a61e1e83db36406b
Robinson Mukena Zevo <romukena@f6r8s7.clusters.42paris.fr>
2026-03-19 13:46:27 +0100

    ex02

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 23:21:51 by romukena          #+#    #+#             */
-/*   Updated: 2026/03/19 11:47:24 by romukena         ###   ########.fr       */
+/*   Updated: 2026/03/19 13:32:30 by romukena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,57 +16,29 @@
 #include <limits>
 #include <stdexcept>
 
-template <typename T>
-class Array
-{
-private:
+template <typename T> class Array {
+  private:
 	T *_value;
 	unsigned int _size;
 
-public:
-	Array() : _value(NULL), _size(0) {
-			  };
-	~Array() { delete[] _value; };
-	Array(unsigned int n)
-	{
-		_size = n;
-		_value = new T[n];
-	};
-	Array(const Array &other)
-	{
-		_size = other.size();
-		_value = new T[_size];
-		for (unsigned int i = 0; i < other.size(); i++)
-			_value[i] = other[i];
-	};
-	Array &operator=(const Array &other)
-	{
-		if (this != &other)
-		{
-			delete[] _value;
-			_size = other.size();
-			_value = new T[_size];
-			for (unsigned int i = 0; i < other.size(); i++)
-				_value[i] = other[i];
-		}
-		return *this;
-	};
-	class OutOfRange : public std::exception
-	{
-	public:
-		virtual const char *what() const throw()
-		{
+  public:
+	Array();
+	~Array();
+	Array(unsigned int n);
+	Array(const Array &other);
+	Array &operator=(const Array<T> &other);
+	class OutOfRange : public std::exception {
+	  public:
+		virtual const char *what() const throw() {
 			return "Invalid index : out of range";
 		}
 	};
-	T &operator[](unsigned int index) const
-	{
+	T &operator[](unsigned int index) const {
 		if (index >= this->size())
 			throw OutOfRange();
 		return _value[index];
 	};
-	unsigned int size() const
-	{
+	unsigned int size() const {
 		return _size;
 	};
 };
--- cpp07/ex02/Array.tpp
+++ cpp07/ex02/Array.tpp
@@ -6,7 +6,54 @@
 /*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/19 11:26:57 by romukena          #+#    #+#             */
-/*   Updated: 2026/03/19 11:39:39 by romukena         ###   ########.fr       */
+/*   Updated: 2026/03/19 13:32:10 by romukena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Array.hpp"
+
+template <typename T> Array<T>::Array() : _value(NULL), _size(0){};
+
+template <typename T> Array<T>::~Array() {
+	delete[] _value;
+};
+
+template <typename T> Array<T>::Array(unsigned int n) {
+	_size = n;
+	_value = new T[n];
+};
+
+template <typename T> Array<T>::Array(const Array &other) {
+	_size = other.size();
+	_value = new T[_size];
+	for (unsigned int i = 0; i < other.size(); i++)
+		_value[i] = other[i];
+};
+
+template <typename T>
+Array<T> &Array<T>:: operator=(const Array<T> &other) {
+	if (this != &other) {
+		delete[] _value;
+		_size = other.size();
+		_value = new T[_size];
+		for (unsigned int i = 0; i < other.size(); i++)
+			_value[i] = other[i];
+	}
+	return *this;
+};
+
+// template <typename T>
+
+// template <typename T>
+
+// template <typename T>
+
+// template <typename T>
+
+// template <typename T>
+
+// template <typename T>
+
+// template <typename T>
+
+// template <typename T>
