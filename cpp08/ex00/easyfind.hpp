01cd988be3309610a52fb40b8341511d94d2afd5
Robinson Mukena Zevo <romukena@f4r6s6.clusters.42paris.fr>
2026-03-21 14:51:33 +0100

    correct ex00

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,26 +6,22 @@
 /*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/15 18:36:21 by romukena          #+#    #+#             */
-/*   Updated: 2026/03/16 15:28:26 by romukena         ###   ########.fr       */
+/*   Updated: 2026/03/21 14:49:42 by romukena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
-#include <vector>
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 #include <stdexcept>
+#include <vector>
 
-template <typename T>
-typename T::iterator easyfind(T &container, int n)
-{
-	std::vector<int>::iterator it = find(container.begin(), container.end(), n);
-	if (it == container.end())
-	{
-		throw std::invalid_argument("Don't find");
-	}
-	else
+template <typename T> typename T::iterator easyfind(T &container, int n) {
+	typename T::iterator it = find(container.begin(), container.end(), n);
+	if (it == container.end()) {
+		throw std::invalid_argument("Value not found");
+	} else
 		return it;
 }
 
