f7c5aeb21db9e950c53b544ec7bcaecfb1d4e0f4
mukena_r <robinsonmukena@gmail.com>
2026-03-22 23:21:10 +0100

    start ex00

---
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romukena <romukena@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/22 22:50:48 by romukena          #+#    #+#             */
+/*   Updated: 2026/03/22 22:59:26 by romukena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+class BitcoinExchange
+{
+private:
+	std::vector<std::string> input_data;
+	std::vector<std::string> other_data;
+public:
+	BitcoinExchange(/* args */);
+	~BitcoinExchange();
+};
+
+BitcoinExchange::BitcoinExchange(/* args */)
+{
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+}
